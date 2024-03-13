e741edbfd9b2dc105538b68cd0ea6dbf64d6df15
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-03-12 22:34:33 -0400

    :fire: firebase listo, no hace falta que hagan deploy solo hagan git push y ya y recuerden hacer npm install

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDAaVSdqUZawI_YxEu2gjbJ4N29DeF_hWI",
+  authDomain: "conectaunimet.firebaseapp.com",
+  projectId: "conectaunimet",
+  storageBucket: "conectaunimet.appspot.com",
+  messagingSenderId: "940197259041",
+  appId: "1:940197259041:web:15a1e2c4e31f1c8adc8060"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
