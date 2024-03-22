935b02e0a2f5cc6d226b898e6fdb64328058558f
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-03-22 13:53:44 -0400

    :ambulance: habia un error con el hosting pero ya lo arregle estaba en el main

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Footer_Navbar from './Components/Footer_Navbar'
-import Agrupaciones from './Pages/Agrupaciones'
-import Inicio from './Pages/Inicio'
-import Feedbacks from './Pages/Feedbacks'
-import Register from './Pages/Signup'
-import Register from './Pages/RegistrarAdmin'
-import UserProvider from './UserProvider'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer_Navbar from "./Components/Footer_Navbar";
+import Agrupaciones from "./Pages/Agrupaciones";
+import Inicio from "./Pages/Inicio";
+import Feedbacks from "./Pages/Feedbacks";
+import Register from "./Pages/Signup";
+import RegistrarAdmin from "./Pages/RegistrarAdmin";
+import UserProvider from "./UserProvider";
 
 const browserRouter = createBrowserRouter([
   {
@@ -23,23 +23,25 @@ const browserRouter = createBrowserRouter([
         element: <Agrupaciones />,
       },
       {
-        path:"/feedbacks",
-        element:<Feedbacks/>
+        path: "/feedbacks",
+        element: <Feedbacks />,
       },
-    ]
-  },{
-    path:"/signup",
-    element:<Register/>
-  },{
-        path: "/registraradmin",
-        element: <RegistrarAdmin/>,
-      },
+    ],
+  },
+  {
+    path: "/signup",
+    element: <Register />,
+  },
+  {
+    path: "/registraradmin",
+    element: <RegistrarAdmin />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <UserProvider>
-    <RouterProvider router={browserRouter}/>
+      <RouterProvider router={browserRouter} />
     </UserProvider>
-    
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
