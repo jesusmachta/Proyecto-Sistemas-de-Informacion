229f281a16210f15fcf3c8450864e07dca2c6aac
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-03-22 13:41:16 -0400

    :technologist: ya comence con el admin, le cree un register para registrarlo por primera vez, pero esa pestaña no se va a volver a usar

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,38 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Footer_Navbar from './Components/Footer_Navbar'
-import Agrupaciones from './Pages/Agrupaciones'
-import Inicio from './Pages/Inicio'
-import Feedbacks from './Pages/Feedbacks'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer_Navbar from "./Components/Footer_Navbar";
+import Agrupaciones from "./Pages/Agrupaciones";
+import Inicio from "./Pages/Inicio";
+import Feedbacks from "./Pages/Feedbacks";
 
 const browserRouter = createBrowserRouter([
   {
-    path:"/",
-    element:<Footer_Navbar/>,
-    children:[
+    path: "/",
+    element: <Footer_Navbar />,
+    children: [
       {
-        index: true, 
-        element:<Inicio/>
+        index: true,
+        element: <Inicio />,
       },
       {
-        path:"/agrupaciones",
-        element:<Agrupaciones/>
+        path: "/agrupaciones",
+        element: <Agrupaciones />,
       },
       {
-        path:"/feedbacks",
-        element:<Feedbacks/>
-      }
-    ]
+        path: "/feedbacks",
+        element: <Feedbacks />,
+      },
+      {
+        path: "/registraradmin",
+        element: <RegistrarAdmin />,
+      },
+    ],
   },
+]);
 
-])
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={browserRouter}/>
-  </React.StrictMode>,
-)
+    <RouterProvider router={browserRouter} />
+  </React.StrictMode>
+);
