edcd47b0b8dcfa0be36acb0c7ed7329f10c4ad42
Federica <g.federica@correo.unimet.edu.ve>
2024-03-25 22:18:27 -0400

    feedback

--- src/Pages/Agrupacion/Agrupacion.jsx
+++ src/Pages/Agrupacion/Agrupacion.jsx
@@ -5,40 +5,53 @@ import { useParams } from "react-router-dom";
 import CarouselAgrupacion from "../../Components/CarouselAgrupacion/CarouselAgrupacion";
 import DropdownInfo from "../../Components/DropdownInfo/DropdownInfo";
 import { getAgrupacionById } from "../../controllers/Agrupaciones";
+import Feedbacks from "../Feedbacks/Feedbacks";
 
 function Agrupacion() {
-
   const [agrupacion, setAgrupacion] = useState(null);
 
   let { id } = useParams();
 
   useEffect(() => {
     if (id) {
-      const buscarId = async () => { 
+      const buscarId = async () => {
         const currentAgrup = await getAgrupacionById(id);
-        console.log(currentAgrup)
-        if (currentAgrup){
+        console.log(currentAgrup);
+        if (currentAgrup) {
           setAgrupacion(currentAgrup);
         }
-      } 
-      buscarId();    
+      };
+      buscarId();
     }
   }, []);
 
-  return ( agrupacion && ( 
-  <div className="agrupacion__container">
-  <BannerAgrupacion title={agrupacion?.name} imgSrc={agrupacion?.ImgSrc} />
-  <div className="description__wrapper">
-    <p>{agrupacion?.description}</p>
-  </div>
-  <CarouselAgrupacion imgsList={agrupacion?.ImgExtras}/>
-  <div className="btns_list">
-    <DropdownInfo title="Mision" info={agrupacion?.mision}/>
-    <DropdownInfo title="Vision" info={agrupacion?.vision}/>
-    <DropdownInfo title="Integrantes" info={agrupacion?.members} isList={true}/>
-  </div>
-</div>)
-   
+  return (
+    agrupacion && (
+      <div className="agrupacion__container">
+        <BannerAgrupacion
+          title={agrupacion?.name}
+          imgSrc={agrupacion?.ImgSrc}
+        />
+        <div className="description__wrapper">
+          <p>{agrupacion?.description}</p>
+        </div>
+        <CarouselAgrupacion imgsList={agrupacion?.ImgExtras} />
+        <div className="btns_list">
+          <DropdownInfo title="Mision" info={agrupacion?.mision} />
+          <DropdownInfo title="Vision" info={agrupacion?.vision} />
+          <DropdownInfo
+            title="Integrantes"
+            info={agrupacion?.members}
+            isList={true}
+          />
+        </div>
+        <div className="buttonUnirseWrapper">
+          <button className="buttonUnirse"> Unirse a Agrupación </button>
+        </div>
+
+        <Feedbacks />
+      </div>
+    )
   );
 }
 
---
+++ src/Pages/Feedbacks/Feedbacks.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { FaRegStar } from "react-icons/fa";
+import { FaStar } from "react-icons/fa";
+import { FaStarHalfAlt } from "react-icons/fa";
+import "./FeedBacks.css";
+import { IoMdAdd } from "react-icons/io";
+import { IoIosArrowRoundForward } from "react-icons/io";
+import Comment from "../../Components/Comment/Comment";
+
+const Feedbacks = () => {
+  return (
+    <div className="FeedbacksContainer">
+      <div className="ratingContainer">
+        <div className="ratingTitleContainer">
+          <p>Rating Agrupación</p>
+        </div>
+        <div className="ratingWrapper">
+          <FaStar size={40} color="#EC9900" />
+          <FaStar size={40} color="#EC9900" />
+          <FaStarHalfAlt size={40} color="#EC9900" />
+          <FaRegStar size={40} color="#EC9900" />
+          <FaRegStar size={40} color="#EC9900" />
+        </div>
+      </div>
+      <div className="contenedorComentario">
+        <div className="boton">
+          <IoMdAdd color="#667085" />
+          <p>Agregar comentario</p>
+        </div>
+        <div className="commentsList">
+          <Comment />
+          <Comment />
+          <Comment />
+        </div>
+        <div className="boton align-right">
+          <p>Ver más</p>
+          <IoIosArrowRoundForward size={25} color="#667085" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Feedbacks;
