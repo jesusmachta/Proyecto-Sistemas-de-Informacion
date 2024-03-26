c33da5a9467b5d5c2ecbc3490c84a16c4877f941
Jesus Machta <97565151+jesusmachta@users.noreply.github.com>
2024-03-25 21:20:28 -0400

    :ambulance: estoy haciendo el login, ya sirve me falta es el css

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {createBrowserRouter, RouterProvider} from 'react-router-dom'
-import Footer_Navbar from './Components/Footer_Navbar'
-import Agrupaciones from './Pages/Agrupaciones'
-import Inicio from './Pages/Inicio'
-import Feedbacks from './Pages/Feedbacks'
-import Register from './Pages/Signup'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer_Navbar from "./Components/Footer_Navbar";
+import Agrupaciones from "./Pages/Agrupaciones";
+import Inicio from "./Pages/Inicio";
+import Feedbacks from "./Pages/Feedbacks";
+import Register from "./Pages/Signup";
 import RegistrarAdmin from "./Pages/RegistrarAdmin";
-import UserProvider from './UserProvider'
-import StudentProfile from './Pages/StudentProfile'
-import SidebarStudent from './Components/SidebarStudent'
+import UserProvider from "./UserProvider";
+import StudentProfile from "./Pages/StudentProfile";
+import SidebarStudent from "./Components/SidebarStudent";
 import UserProvider from "./UserProvider";
 import { LoginAdmin } from "./Pages/loginAdmin";
-
 
 const browserRouter = createBrowserRouter([
   {
@@ -31,26 +30,24 @@ const browserRouter = createBrowserRouter([
         path: "/feedbacks",
         element: <Feedbacks />,
       },
-    ]
-  },{
-    path:"/signup",
-    element:<Register/>
-  },{
+    ],
+  },
+  {
+    path: "/signup",
+    element: <Register />,
+  },
+  {
     // borrar!!!!
-    path: "profile", 
-    element: <StudentProfile/>
-  },{
-    path: "sidebarP", 
-    element: <SidebarStudent/>
-    
-  },   
-   {
+    path: "profile",
+    element: <StudentProfile />,
+  },
+  {
+    path: "sidebarP",
+    element: <SidebarStudent />,
+  },
+  {
     path: "/registraradmin",
     element: <RegistrarAdmin />,
-  },
-   {
-    path: "/loginadmin",
-    element: <LoginAdmin />,
   },
   {
     path: "/loginadmin",
