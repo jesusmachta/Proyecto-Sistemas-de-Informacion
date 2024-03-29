8d32f744a01f4918ee66718d14761fac8b0874cd
OrveoDiLuca <102887303+OrveoDiLuca@users.noreply.github.com>
2024-03-29 12:12:33 -0400

    Arreglado

--- src/Pages/Feedbacks/Feedbacks.jsx
+++ src/Pages/Feedbacks/Feedbacks.jsx
@@ -2,19 +2,19 @@ import React from "react";
 import { FaRegStar } from "react-icons/fa";
 import { FaStar } from "react-icons/fa";
 import { FaStarHalfAlt } from "react-icons/fa";
-import styles from "./Feedbacks.css";
+import styles from "./Feedbacks.module.css";
 import { IoMdAdd } from "react-icons/io";
 import { IoIosArrowRoundForward } from "react-icons/io";
 import Comment from "../../Components/Comment/Comment";
 
 const Feedbacks = () => {
   return (
-    <div className="FeedbacksContainer">
-      <div className="ratingContainer">
-        <div className="ratingTitleContainer">
+    <div className={styles.FeedbacksContainer}>
+      <div className={styles.ratingContainer}>
+        <div className={styles.ratingTitleContainer}>
           <p>Rating Agrupación</p>
         </div>
-        <div className="ratingWrapper">
+        <div className={styles.ratingWrapper}>
           <FaStar size={40} color="#EC9900" />
           <FaStar size={40} color="#EC9900" />
           <FaStarHalfAlt size={40} color="#EC9900" />
@@ -22,17 +22,17 @@ const Feedbacks = () => {
           <FaRegStar size={40} color="#EC9900" />
         </div>
       </div>
-      <div className="contenedorComentario">
-        <div className="boton">
+      <div className={styles.contenedorComentario}>
+        <div className={styles.boton}>
           <IoMdAdd color="#667085" />
           <p>Agregar comentario</p>
         </div>
-        <div className="commentsList">
+        <div className={styles.commentsList}>
           <Comment />
           <Comment />
           <Comment />
         </div>
-        <div className="boton align-right">
+        <div className={`${styles.boton} alignRight`}>
           <p>Ver más</p>
           <IoIosArrowRoundForward size={25} color="#667085" />
         </div>
