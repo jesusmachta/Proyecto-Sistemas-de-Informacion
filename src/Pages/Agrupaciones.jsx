70394f4e481c4b181b782353f8d5a15c1ec72c95
danielaparrac <104109684+danielaparrac@users.noreply.github.com>
2024-03-29 13:05:47 -0400

    searchbar listo
    ya listo

--- src/Pages/Agrupaciones.jsx
+++ src/Pages/Agrupaciones.jsx
@@ -1,13 +1,48 @@
-import styles from "./Agrupaciones.module.css"
+import styles from "./Agrupaciones.module.css";
 import Card from "../Components/Card";
+import { useState, useEffect } from "react";
+import { db, storage } from "../firebase";
+import { collection, getDocs } from "firebase/firestore";
+import { listAll, ref, getDownloadURL } from "firebase/storage";
+import SearchBar from "../Components/SearchBar/SearchBar";
 
 const Agrupaciones = () => {
+  const [agrupaciones, setAgrupaciones] = useState([]);
+  const [filteredAgrupaciones, setFilteredAgrupaciones] = useState([]);
+  const imagenesRef = ref(storage, "Agrupaciones/");
+  const agrupacionesRef = collection(db, "Agrupaciones");
+
+  useEffect(() => {
+    const obtenerAgrupaciones = async () => {
+      try {
+        const querySnapshot = await getDocs(agrupacionesRef);
+
+        const listAgrupaciones = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          nombre: doc.data().name,
+          descripcion: doc.data().description,
+        }));
+        setAgrupaciones(listAgrupaciones);
+        setFilteredAgrupaciones(listAgrupaciones);
+      } catch (error) {
+        console.error("Error al obtener las agrupaciones:", error);
+      }
+    };
+
+    obtenerAgrupaciones();
+  }, []);
+
   return (
     <div>
       <h1 className={styles.title}>Agrupaciones</h1>
-      <Card />
+      <SearchBar agrupaciones={agrupaciones} setFilteredAgrupaciones={setFilteredAgrupaciones}/>
+      <div className={styles.card}>
+        {filteredAgrupaciones.map((agrupacion) => (
+          <Card key={agrupacion.id} agrupacion={agrupacion} />
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Agrupaciones
+export default Agrupaciones;
