9ae5409965e229e571148710c1b8b1827feafc23
Catalina Matheus <catalina.matheus@correo.unimet.edu.ve>
2024-03-30 17:03:41 -0400

    agregados botones de seleccion de estudiante o administrador en el login

--- src/Pages/loginAdmin.jsx
+++ src/Pages/loginAdmin.jsx
@@ -16,6 +16,7 @@ export function LoginAdmin() {
   const [errorMsg, setErrorMsg] = useState([]);
   const [submitButtonDisabled, setSubmitButtonDisabled] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
+  const [selectedButton, setSelectedButton] = useState(null);
   const Iniciar = () => {
     if (!values.email || !values.password) {
       setErrorMsg("Datos incompletos");
@@ -60,10 +61,28 @@ export function LoginAdmin() {
         <img src={logo} alt="logo" className={styles.logo}></img>
       </div>
       <div className={styles.buttonsLogInSignUp}>
-        <button className={`${styles.button} ${styles.buttonSelected}`}>Log In</button>
+        <button className={`${styles.button} ${styles.buttonSelected}`}>
+          Log In
+        </button>
         <button className={styles.button} onClick={() => navigate("/signup")}>
           Sign Up
         </button>
+      </div>
+      <div className={styles.buttonsContainer}>
+        <button
+          className={`${styles.button2} ${
+            selectedButton === "student" ? styles.selected : ""
+          }`}
+          onClick={() => setSelectedButton("student")}
+        ></button>
+        <h3 className={styles.textobotones}>Estudiante</h3>
+        <button
+          className={`${styles.button2} ${
+            selectedButton === "admin" ? styles.selected : ""
+          }`}
+          onClick={() => setSelectedButton("admin")}
+        ></button>
+        <h3 className={styles.textobotones}>Administrador</h3>
       </div>
       <b className={styles.error}>{errorMsg}</b>
       <form className={styles.container}>
@@ -89,7 +108,11 @@ export function LoginAdmin() {
           showPassword={showPassword}
           onTogglePassword={() => setShowPassword(!showPassword)}
         />
-        <button onClick={Iniciar} disabled={submitButtonDisabled} className={styles.buttonIniciar}>
+        <button
+          onClick={Iniciar}
+          disabled={submitButtonDisabled}
+          className={styles.buttonIniciar}
+        >
           Iniciar
         </button>
       </form>
