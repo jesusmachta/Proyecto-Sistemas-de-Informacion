0e7459a423ecd1fff7718baa774c8a9b1ee5e667
Federicagrossmann <g.federica@correo.unimet.edu.ve>
2024-03-30 12:38:36 -0400

    faltaba un import en main

--- src/main.jsx
+++ src/main.jsx
@@ -18,6 +18,7 @@ import { LoginAdmin } from "./Pages/loginAdmin";
 import UpdateGroup from "./Pages/UpdateGroup";
 import RegisterGroup from "./Pages/RegisterGroup";
 import AdminProfile from "./Pages/AdminProfile";
+import DeleteGroup from "./Pages/DeleteGroup";
 
 const browserRouter = createBrowserRouter([
   {
