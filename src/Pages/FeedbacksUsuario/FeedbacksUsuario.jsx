a59f97e3fd05ea0aacff2e443829018bea0ebce9
Federicagrossmann <g.federica@correo.unimet.edu.ve>
2024-03-31 11:43:14 -0400

    mostrar en select solo agrupaciones a las que pertenece

--- src/Pages/FeedbacksUsuario/FeedbacksUsuario.jsx
+++ src/Pages/FeedbacksUsuario/FeedbacksUsuario.jsx
@@ -7,6 +7,7 @@ import { FaStarHalfAlt } from 'react-icons/fa';
 import { getAllAgrupaciones } from '../../controllers/Agrupaciones';
 import { useUser } from '../../context/user';
 import { createFeedback, getUserFeedbacks } from '../../controllers/feedbacks';
+import { getStudentById } from '../../controllers/updateUser';
 
 function FeedbacksUsuario() {
   const [data, setData] = useState([]);
@@ -15,6 +16,8 @@ function FeedbacksUsuario() {
   const [feedbackInput, setFeedBackInput] = useState('');
   const [rating, setRating] = useState(null);
   const [hover, setHover] = useState(null);
+  const [studentData, setStudentData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const user = useUser();
 
@@ -26,14 +29,23 @@ function FeedbacksUsuario() {
   };
 
   useEffect(() => {
+    setLoading(true);
     if(user){
-      const handleGetAgrupaciones = async () => {
+      const handleGetStudentAgrupaciones = async () => {
         const agrupacionesList = await getAllAgrupaciones();
   
         if (agrupacionesList) {
-          setData(agrupacionesList);
+          const student = await getStudentById(user.uid);
+          setStudentData(student)
+          const myAgrupaciones = student.afiliaciones.map(afiliacion => {
+            const found = agrupacionesList.find(agrupacion => agrupacion?.name === afiliacion?.nombre)
+            if(found){
+              return found
+            }
+          });
+          setData(myAgrupaciones)
         }
-      };
+      }
   
       const handleGetMyFeedbacks = async() => {
         const myFeedbacksList = await getUserFeedbacks(user.email)
@@ -43,8 +55,9 @@ function FeedbacksUsuario() {
         }
       }
   
-      handleGetAgrupaciones();
-      handleGetMyFeedbacks()
+      handleGetStudentAgrupaciones()
+      handleGetMyFeedbacks();
+      setLoading(false);
     }
   }, []);
 
@@ -84,61 +97,68 @@ function FeedbacksUsuario() {
     }
   };
 
-  return (
-    <div className={styles.Contenedor}>
-      <div className={styles.UsuarioHeader}>
-        <IoPersonCircle size={70} />
-        <div>
-          <p className={styles.username}>Jane Doe</p>
-          <p>jane.doe@correo.unimet.edu.ve</p>
+  if(loading && !user && !studentData){
+    return (
+      <p>Loading...</p>
+    )
+  }else{
+    return (
+      <div className={styles.Contenedor}>
+        <div className={styles.UsuarioHeader}>
+          <IoPersonCircle size={70} />
+          <div>
+            <p className={styles.username}>{`${studentData?.name} ${studentData?.lastName}`}</p>
+            <p>{user?.email}</p>
+          </div>
         </div>
-      </div>
-      <div className={styles.feedbacksWrapper}>
-        <div className={styles.ParteIzquierda}>
-          <p>Agrupaciones a las que perteneces:</p>
-          <select
-            className={styles.Agrupaciones}
-            value={selectedData}
-            onChange={handleSelectData}
-            disabled={disabled}
-          >
-            <option disabled value=''>
-              Agrupaciones
-            </option>
-            {data?.map((element, index) => (
-              <option key={index} value={element.name}>
-                {element.name}
+        <div className={styles.feedbacksWrapper}>
+          <div className={styles.ParteIzquierda}>
+            <p>Agrupaciones a las que perteneces:</p>
+            <select
+              className={styles.Agrupaciones}
+              value={selectedData}
+              onChange={handleSelectData}
+              disabled={disabled}
+            >
+              <option disabled value=''>
+                Agrupaciones
               </option>
-            ))}
-          </select>
-        </div>
-        <div className={styles.ParteDerecha}>
-          <textarea
-            className={styles.textArea}
-            value={feedbackInput}
-            id = "feedback"
-            onChange={(e) => setFeedBackInput(e.target.value)}
-          ></textarea>
-          <div className={styles.Rating}>
-            <button onClick={handleCreateFeedback}>
-              Pulsa para valorar la agrupación
-            </button>
-            <div className={styles.Estrellas}>
-              {[...Array(5)].map((star, index) => {
-                const currentRating = index + 1
-                return (
-                  <label key={index}>
-                    <input type='radio' name='rating' value={currentRating} onClick={() => setRating(currentRating)}/>
-                    <FaStar className={styles.star} size={40} color={currentRating <= (hover || rating) ? '#ffc107' : '#D0D5DD'} onMouseEnter={() => setHover(currentRating)} onMouseLeave={() => setHover(null)}/>
-                  </label>
-                )
-              })}
+              {data?.map((element, index) => (
+                <option key={index} value={element.name}>
+                  {element.name}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div className={styles.ParteDerecha}>
+            <textarea
+              className={styles.textArea}
+              value={feedbackInput}
+              id = "feedback"
+              onChange={(e) => setFeedBackInput(e.target.value)}
+            ></textarea>
+            <div className={styles.Rating}>
+              <button onClick={handleCreateFeedback}>
+                Pulsa para valorar la agrupación
+              </button>
+              <div className={styles.Estrellas}>
+                {[...Array(5)].map((star, index) => {
+                  const currentRating = index + 1
+                  return (
+                    <label key={index}>
+                      <input type='radio' name='rating' value={currentRating} onClick={() => setRating(currentRating)}/>
+                      <FaStar className={styles.star} size={40} color={currentRating <= (hover || rating) ? '#ffc107' : '#D0D5DD'} onMouseEnter={() => setHover(currentRating)} onMouseLeave={() => setHover(null)}/>
+                    </label>
+                  )
+                })}
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
+
 }
 
 export default FeedbacksUsuario;
